6f17de2b8e9ef23a1824800735324b68f130c84b
phpMyYang <jeremiahoccupation@gmail.com>
2026-03-04 13:25:01 +0800

    Initial commit: Setup backend project structure

---
+++ app/Rules/Recaptcha.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Support\Facades\Http;
+
+class Recaptcha implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        // I-send ang request sa Google reCAPTCHA endpoint
+        $response = Http::asForm()->post('https://www.google.com/recaptcha/api/siteverify', [
+            'secret' => env('RECAPTCHA_SECRET_KEY'),
+            'response' => $value,
+            'remoteip' => request()->ip()
+        ]);
+
+        // Kapag false ang success, i-trigger ang error
+        if (! $response->json('success')) {
+            $fail('The reCAPTCHA verification failed. Please try again.');
+        }
+    }
+}
